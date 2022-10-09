c8acbbe9cc95cb4920ee5db522bca630fa291593
Marcos <marcossilvatl2@gmail.com>
2022-10-09 12:29:07 -0400

    refactor: changed location of svgs

--- src/components/SidebarProductCard/index.tsx
+++ src/components/SidebarProductCard/index.tsx
@@ -1,34 +1,13 @@
+import { Add } from '@/assets/Add';
+import { Subtract } from '@/assets/Subtract';
 import { formatCurrency } from '@/helpers';
+
 import { useCartStore } from '@/store';
 import { ProductCart } from '@/types';
 
 interface SidebarProductCardProps {
   product: ProductCart;
 }
-
-const Add = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width="24"
-    height="24"
-  >
-    <path fill="none" d="M0 0h24v24H0z" />
-    <path d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z" />
-  </svg>
-);
-
-const Subtract = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width="24"
-    height="24"
-  >
-    <path fill="none" d="M0 0h24v24H0z" />
-    <path d="M5 11h14v2H5z" />
-  </svg>
-);
 
 import './styles.css';
 
